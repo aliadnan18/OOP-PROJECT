b435c8431658b7daa789eb9c844d7c1afed65ce1
Ali Adnan <aliadnanx16@gmail.com>
2026-03-27 19:44:39 +0500

    completed functions updateFog, isWalkable, and chasePlayer. Also changed maxWidth to be non-const so it can be used in updateFog.

--- headers1.hpp
+++ headers1.hpp
@@ -63,7 +63,7 @@ class Enemy : public Character{
     int aggroRadius;
 public:
     Enemy(int x, int y) : Character(x, y, 'E', 50), aggroRadius(5) {}
-    void chasePlayer(int px, int py);
+    void chasePlayer(int px, int py){}
 };
 
 
@@ -77,10 +77,20 @@ public:
     vector<unique_ptr<Item>> items;
     vector<unique_ptr<Enemy>> enemies;
     sf::Vector2i entrance, exit;
-    Floor(int level){};
-    void generateLayout(){};
-    void updateFog(int px, int py){};
-    bool isWalkable(int x, int y) const {};
+    Floor(int level){}
+    void generateLayout(){}
+    void updateFog(int px, int py){
+        for(int dy=-1; dy<=1; dy++) {
+            for(int dx=-1; dx<=1; dx++) {
+                int nx = px + dx; int ny = py + dy;
+                if(nx >=0 && nx < maxWidth && ny >=0 && ny < maxHeight) 
+                    fog[ny][nx] = false;
+            }
+        }
+    }
+    bool isWalkable(int x, int y) const {
+        return grid[y][x] != TileType::WALL;
+    }
     TileType getTile(int x, int y) const {return grid[y][x];}
     bool isVisible(int x, int y) const {return !fog[y][x];}
 };
