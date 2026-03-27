8be8b6df3f9652aaa3e9157ae5151dbbe4a725ab
Ali Adnan <aliadnanx16@gmail.com>
2026-03-27 20:10:38 +0500

    added floor 2 functions

--- Entity.cpp
+++ Entity.cpp
@@ -2,7 +2,22 @@
 #include <iostream>
 
 using namespace std;
-
+// potion functions
 void Potion::use(Character& target) {
     target.heal(healAmount); 
 }
+// floor functions
+void Floor::updateFog(int px, int py){
+        for(int dy=-1; dy<=1; dy++) {
+            for(int dx=-1; dx<=1; dx++) {
+                int nx = px + dx; int ny = py + dy;
+                if(nx >=0 && nx < maxWidth && ny >=0 && ny < maxHeight) 
+                    fog[ny][nx] = false;
+            }
+        }
+    }
+
+
+bool Floor::isWalkable(int x, int y) const{
+        return grid[y][x] != TileType::WALL;
+    }
