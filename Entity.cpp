d021a6b013aab5006ab56b7fcddfaa2285897b6d
Ali Adnan <aliadnanx16@gmail.com>
2026-03-28 22:28:20 +0500

    m

--- Entity.cpp
+++ Entity.cpp
@@ -1,5 +1,6 @@
 #include "Entity.hpp" 
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 // potion functions
@@ -48,4 +49,27 @@ void Floor::generateLayout(){
             
 
 }
+    }
+
+// seperate functions 
+void processInput() {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+        if (inputTimer.getElapsedTime().asMilliseconds() < 150) return;
+        int dx = 0, dy = 0;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) dy = -1;
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) dy = 1;
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) dx = -1;
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) dx = 1;
+        if (dx != 0 || dy != 0) {
+            int nx = pPos.x + dx;
+            int ny = pPos.y + dy;
+            if (nx >= 0 && nx < MAX_W && ny >= 0 && ny < MAX_H && currentFloor->grid[ny][nx] != WALL) {
+                pPos = {nx, ny};
+                currentFloor->reveal(pPos.x, pPos.y);
+                inputTimer.restart();
+            }
+        }
     }
